0effbb5ea64733d6c8c9f64fd10c32fbaff77245
Bartłomiej Szałach <onegrx@gmail.com>
2016-11-23 23:55:02 +0100

    Add DHT11 sensor

--- src/hydroplant.ino
+++ src/hydroplant.ino
@@ -1,3 +1,8 @@
+#include <dht11.h>
+
+dht11 DHT11;
+#define dataPin 2
+
 void setup(){
   Serial.begin(57600);
 }
@@ -6,8 +11,20 @@ void loop(){
 
   int moisture = analogRead(A0);
 
+  int dhtData = DHT11.read(dataPin);
+  float t = DHT11.temperature;
+  float h = DHT11.humidity;
 
   Serial.print("Moisture Sensor Value: ");
   Serial.println(moisture);
-  delay(500);
+
+  Serial.print("Temperature Value: ");
+  Serial.println(t);
+
+  Serial.print("Humidity Value: ");
+  Serial.println(h);
+
+  Serial.println();
+
+  delay(1000);
 }
