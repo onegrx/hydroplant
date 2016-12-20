275a5970174c77d9423d66e5189f492a128cfc41
hector <krzysiuk.66@gmail.com>
2016-12-20 16:08:33 +0100

    simple algorithm

--- src/hydroplant/hydroplant.ino
+++ src/hydroplant/hydroplant.ino
@@ -1,8 +1,10 @@
-#include "../lib/dht11/dht11.h"
-#include "../lib/rgb_color/rgb_color.h"
-#include "../lib/fader/fader.h"
+#include "../../lib/dht11/dht11.h"
+#include "../../lib/rgb_color/rgb_color.h"
+#include "../../lib/fader/fader.h"
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <dht11.h>
+#include <time.h>
 
 dht11 DHT11;
 LiquidCrystal lcd(9, 8, A2, A3, A4, A5);
@@ -10,12 +12,24 @@ LiquidCrystal lcd(9, 8, A2, A3, A4, A5);
 const int dhtPin = 2;
 const int moisturePin = A0;
 const int lightPin = A1;
+const int thresholdMoisture = 900;
+const int minMoisture = 500;
+const int timeout = 1000;           //todo change to an hour
+const int wateringTimeout = 100;    //todo change to a minute
+
+
+char *getCurrentTime();
+
+void waterOneUnit();
+
+int readMoisture() ;
 const int redPin = 6;
 const int greenPin = 5;
 const int bluePin = 3;
 const int pumpPin = 7;
 
 rgb_color current(0, 0, 0);
+
 
 void setup() {
 
@@ -35,7 +49,7 @@ void setup() {
   setColor(0, 0, 0);
 }
 
-void loop(){
+void loop() {
 
   //Initialize colors
   fader f (redPin, greenPin, bluePin);
@@ -45,25 +59,24 @@ void loop(){
 
   int moisture = analogRead(moisturePin);
 
-  int dhtData = DHT11.read(dhtPin);
-  float t = DHT11.temperature;
-  float h = DHT11.humidity;
+    int currentMoisture = readMoisture();
+    Serial.print("current moisture: ");
+    Serial.println(currentMoisture);
 
-  int light = analogRead(lightPin);
+    if (currentMoisture > thresholdMoisture) {
+        Serial.println("i'm inside if");
 
-  //Serial print
-  Serial.print("Moisture Sensor Value: ");
-  Serial.println(moisture);
+        do {
+            Serial.print("water time: ");
+            Serial.println(millis());
 
-  Serial.print("Temperature Value: ");
-  Serial.println(t);
+            waterOneUnit();
 
-  Serial.print("Humidity Value: ");
-  Serial.println(h);
+            delay(wateringTimeout);
+            currentMoisture = readMoisture();
 
-  Serial.print("Light Value: ");
-  Serial.println(light);
-
+        } while (currentMoisture > minMoisture);
+    }
   Serial.println();
 
   printToLcd(t, h, moisture, light);
@@ -91,21 +104,29 @@ void loop(){
 }
 
 int readTemperature() {
-  int dhtData = DHT11.read(dhtPin);
-  return (int) DHT11.temperature;
+    int dhtData = DHT11.read(dhtPin);
+    return (int) DHT11.temperature;
 }
 
 int readHumidity() {
-  int dhtData = DHT11.read(dhtPin);
-  return (int) DHT11.humidity;
+    int dhtData = DHT11.read(dhtPin);
+    return (int) DHT11.humidity;
 }
 
 int readMoisture() {
-  return (int) analogRead(moisturePin);
+    return (int) analogRead(moisturePin);
+//    const int numberOfMoistureSamples = 15;
+//    const int moistureSamplingTimeout = 1000;
+//    int moistureSamplesSum = 0;
+//    for (int i = 0; i < numberOfMoistureSamples; i++) {
+//        moistureSamplesSum += (int) analogRead(moisturePin);
+//        delay(moistureSamplingTimeout);
+//    }
+//    return moistureSamplesSum / numberOfMoistureSamples;
 }
 
 int readLight() {
-  return (int) analogRead(lightPin);
+    return (int) analogRead(lightPin);
 }
 
 void setColor(int red, int green, int blue) {
