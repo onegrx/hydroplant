a747b0543bdc0bc19c13677f0548e9aedad6fa3f
onegrx <onegrx@gmail.com>
2017-01-06 21:59:20 +0100

    DHT11 library

--- lib/dht11/dht11.cpp
+++ lib/dht11/dht11.cpp
@@ -1,16 +1,23 @@
 #include "dht11.h"
 
-int ackOrTimeout(int pin);
+// There is a timeout if voltage on pin does not change in 10k loop passes time
+
+bool ack(int pin, int mode) {
+	for(unsigned int loopCnt = 10000; digitalRead(pin) == mode; loopCnt--) {
+		if (loopCnt <= 0) return false;
+	}
+	return true;
+}
 
 int dht11::read(int pin) {
-	// BUFFER TO RECEIVE
+
 	uint8_t bits[5];
 	uint8_t cnt = 7;
 	uint8_t idx = 0;
 
-	for (int i = 0; i < 5; ++i) {
-    bits[i] = 0;
-  }
+	for (int i = 0; i < 5; i++) {
+		bits[i] = 0;
+	}
 
 	// REQUEST SAMPLE
 	pinMode(pin, OUTPUT);
@@ -20,33 +27,36 @@ int dht11::read(int pin) {
 	delayMicroseconds(40);
 	pinMode(pin, INPUT);
 
-	// ACKNOWLEDGE or TIMEOUT
-  if(ackOrTimeout(pin) != 0) return DHTLIB_ERROR_TIMEOUT;
+	if(!ack(pin, LOW)) return DHTLIB_ERROR_TIMEOUT;
+	if(!ack(pin, HIGH)) return DHTLIB_ERROR_TIMEOUT;
 
-	// READ OUTPUT - 40 BITS => 5 BYTES or TIMEOUT
-	for (int i=0; i<40; i++)
-	{
-		int counter = 10000;
-		while(digitalRead(pin) == LOW)
-			if (counter-- == 0) return DHTLIB_ERROR_TIMEOUT;
+	// DHT sends 5 bytes ie. 40 bits as output
+	for (int i = 0; i < 40; i++) {
+
+		if(!ack(pin, LOW)) return DHTLIB_ERROR_TIMEOUT;
 
 		unsigned long t = micros();
 
-		counter = 10000;
-		while(digitalRead(pin) == HIGH)
-			if (counter-- == 0) return DHTLIB_ERROR_TIMEOUT;
+		if(!ack(pin, HIGH)) return DHTLIB_ERROR_TIMEOUT;
 
-		if ((micros() - t) > 40) bits[idx] |= (1 << cnt);
-		if (cnt == 0)   // next byte?
-		{
-			cnt = 7;    // restart at MSB
-			idx++;      // next byte!
+		// Below is 0, above is 1
+		if((micros() - t) > 40) {
+			bits[idx] |= (1 << cnt);
 		}
-		else cnt--;
+
+		// If counter = 0 move to the next byte,
+		// otherwise decrese counter to set next bit of requested bits
+
+		if(cnt == 0) {
+			cnt = 7;
+			idx++;
+		}	else {
+			cnt--;
+		}
+
 	}
 
-	// WRITE TO RIGHT VARS
-        // as bits[1] and bits[3] are allways zero they are omitted in formulas.
+  // Since bits[1] and bits[3] are always zero they are omitted
 	humidity    = bits[0];
 	temperature = bits[2];
 
@@ -54,18 +64,5 @@ int dht11::read(int pin) {
 
 	if (bits[4] != sum) return DHTLIB_ERROR_CHECKSUM;
 	return DHTLIB_OK;
-}
 
-int ackOrTimeout(int pin) {
-
-  int counter;
-  counter  = 10000;
-	while(digitalRead(pin) == LOW)
-		if (counter-- == 0) return DHTLIB_ERROR_TIMEOUT;
-
-	counter = 10000;
-	while(digitalRead(pin) == HIGH)
-		if (counter-- == 0) return DHTLIB_ERROR_TIMEOUT;
-
-  return 0;
 }
