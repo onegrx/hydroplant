37bd41ac7bda425e55b761d2335f2ccce4fb01ce
onegrx <onegrx@gmail.com>
2017-01-07 22:34:28 +0100

    Add LCD display

--- src/hydroplant.ino
+++ src/hydroplant.ino
@@ -1,8 +1,11 @@
 #include "../lib/dht11/dht11.h"
 #include "../lib/rgb_color/rgb_color.h"
 #include "../lib/fader/fader.h"
+#include <Wire.h>
+#include <LiquidCrystal.h>
 
 dht11 DHT11;
+LiquidCrystal lcd(9, 8, A2, A3, A4, A5);
 
 const int dhtPin = 2;
 const int moisturePin = A0;
@@ -12,9 +15,14 @@ const int greenPin = 5;
 const int bluePin = 3;
 const int pumpPin = 7;
 
-rgb_color current( 0, 0,  0 );
+rgb_color current(0, 0, 0);
 
 void setup() {
+
+  lcd.begin(16, 2);
+  lcd.print("*  Hydroplant  *");
+  delay(1000);
+  lcd.print("");
 
   Serial.begin(57600);
 
@@ -29,8 +37,8 @@ void setup() {
 
 void loop(){
 
+  //Initialize colors
   fader f (redPin, greenPin, bluePin);
-
   rgb_color red(255, 0, 0);
   rgb_color blue(0, 0, 255);
   rgb_color green(0, 255, 0);
@@ -43,6 +51,7 @@ void loop(){
 
   int light = analogRead(lightPin);
 
+  //Serial print
   Serial.print("Moisture Sensor Value: ");
   Serial.println(moisture);
 
@@ -56,6 +65,9 @@ void loop(){
   Serial.println(light);
 
   Serial.println();
+
+  printToLcd(t, h, moisture, light);
+
 
   if(t > 21) {
     digitalWrite(pumpPin, HIGH);
@@ -97,7 +109,6 @@ int readLight() {
 }
 
 void setColor(int red, int green, int blue) {
-
   //Using common anode
   red = 255 - red;
   green = 255 - green;
@@ -106,4 +117,17 @@ void setColor(int red, int green, int blue) {
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);
+}
+
+void printToLcd(int t, int h, int m, int l) {
+  lcd.setCursor(0,0);
+  lcd.print("T: ");
+  lcd.print(t);
+  lcd.print("  M: ");
+  lcd.print(m);
+  lcd.setCursor(0,1);
+  lcd.print("H: ");
+  lcd.print(h);
+  lcd.print("  L: ");
+  lcd.print(l);
 }
