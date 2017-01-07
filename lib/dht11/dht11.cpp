0e2411206a64735328c99bc400cb1dd30adf51e2
onegrx <onegrx@gmail.com>
2017-01-07 01:03:48 +0100

    Refactor code

--- lib/dht11/dht11.cpp
+++ lib/dht11/dht11.cpp
@@ -1,8 +1,7 @@
 #include "dht11.h"
 
 // There is a timeout if voltage on pin does not change in 10k loop passes time
-
-bool ack(int pin, int mode) {
+bool acknowledge(int pin, int mode) {
 	for(unsigned int loopCnt = 10000; digitalRead(pin) == mode; loopCnt--) {
 		if (loopCnt <= 0) return false;
 	}
@@ -11,15 +10,15 @@ bool ack(int pin, int mode) {
 
 int dht11::read(int pin) {
 
-	uint8_t bits[5];
-	uint8_t cnt = 7;
-	uint8_t idx = 0;
+	uint8_t buffer[5];
+	uint8_t bit_shift = 7;
+	uint8_t current_byte = 0;
 
 	for (int i = 0; i < 5; i++) {
-		bits[i] = 0;
+		buffer[i] = 0;
 	}
 
-	// REQUEST SAMPLE
+	// Handshake
 	pinMode(pin, OUTPUT);
 	digitalWrite(pin, LOW);
 	delay(18);
@@ -27,42 +26,42 @@ int dht11::read(int pin) {
 	delayMicroseconds(40);
 	pinMode(pin, INPUT);
 
-	if(!ack(pin, LOW)) return DHTLIB_ERROR_TIMEOUT;
-	if(!ack(pin, HIGH)) return DHTLIB_ERROR_TIMEOUT;
+	if(!acknowledge(pin, LOW)) return DHTLIB_ERROR_TIMEOUT;
+	if(!acknowledge(pin, HIGH)) return DHTLIB_ERROR_TIMEOUT;
 
 	// DHT sends 5 bytes ie. 40 bits as output
 	for (int i = 0; i < 40; i++) {
 
-		if(!ack(pin, LOW)) return DHTLIB_ERROR_TIMEOUT;
+		if(!acknowledge(pin, LOW)) return DHTLIB_ERROR_TIMEOUT;
 
 		unsigned long t = micros();
 
-		if(!ack(pin, HIGH)) return DHTLIB_ERROR_TIMEOUT;
+		if(!acknowledge(pin, HIGH)) return DHTLIB_ERROR_TIMEOUT;
 
 		// Below is 0, above is 1
 		if((micros() - t) > 40) {
-			bits[idx] |= (1 << cnt);
+			buffer[current_byte] |= (1 << bit_shift);
 		}
 
 		// If counter = 0 move to the next byte,
 		// otherwise decrese counter to set next bit of requested bits
 
-		if(cnt == 0) {
-			cnt = 7;
-			idx++;
+		if(bit_shift == 0) {
+			bit_shift = 7;
+			current_byte++;
 		}	else {
-			cnt--;
+			bit_shift--;
 		}
 
 	}
 
   // Since bits[1] and bits[3] are always zero they are omitted
-	humidity    = bits[0];
-	temperature = bits[2];
+	humidity    = buffer[0];
+	temperature = buffer[2];
 
-	uint8_t sum = bits[0] + bits[2];
+	uint8_t sum = buffer[0] + buffer[2];
 
-	if (bits[4] != sum) return DHTLIB_ERROR_CHECKSUM;
+	if (buffer[4] != sum) return DHTLIB_ERROR_CHECKSUM;
 	return DHTLIB_OK;
 
 }
