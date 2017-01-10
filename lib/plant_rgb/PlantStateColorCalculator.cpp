eb1fd85225f11e1ef97e025e57122fd91f90c244
hector <krzysiuk.66@gmail.com>
2017-01-11 00:14:23 +0100

    further asserts 2

--- lib/plant_rgb/PlantStateColorCalculator.cpp
+++ lib/plant_rgb/PlantStateColorCalculator.cpp
@@ -5,7 +5,7 @@
 #include "PlantStateColorCalculator.h"
 #include "../rgb_color/rgb_color.h"
 #include <math.h>
-#include <assert.h>
+#include <Arduino.h>
 
 rgb_color PlantStateColorCalculator::calcPlantStateColor(int t, int h, int m) {
 
@@ -23,6 +23,12 @@ rgb_color PlantStateColorCalculator::calcPlantStateColor(int t, int h, int m) {
 
 //private
 
+
+void assert(int line, bool check) {
+  Serial.print(line);
+  Serial.println(check);
+}
+
 /**
  *
  * @param max worst condition
@@ -31,14 +37,14 @@ rgb_color PlantStateColorCalculator::calcPlantStateColor(int t, int h, int m) {
  * @return value mapped to [0, 1] interval where 0 is worst condition and 1 is optimal
  */
 double PlantStateColorCalculator::mapValueToNormalizedInterval(int max, int min, int current) {
-    assert(max > min);
-    assert(current >= min && current <= max);
+    assert(39, max > min);
+    assert(40, current >= min && current <= max);
     double optimalValue = (max + min) / 2;
     double badToOptimal = (max - min) / 2;
     double distFromOptimal = fabs(optimalValue - current);
 
     double result = distFromOptimal >= badToOptimal ? 0 : (1 - distFromOptimal) / badToOptimal;
-    assert(result <= 1.0 && result >= 0.0);
+    assert(46, result <= 1.0 && result >= 0.0);
     return result;
 }
 
@@ -50,9 +56,9 @@ double PlantStateColorCalculator::mapValueToNormalizedInterval(int max, int min,
  * @return all params average normalized on [0, 1] interval
  */
 double PlantStateColorCalculator::normalizeInputValues(double temperature, double humidity, double moisture) {
-    assert(temperature <= 1.0 && temperature >= 0.0);
-    assert(humidity <= 1.0 && humidity >= 0.0);
-    assert(moisture <= 1.0 && moisture >= 0.0);
+    assert(58, temperature <= 1.0 && temperature >= 0.0);
+    assert(59, humidity <= 1.0 && humidity >= 0.0);
+    assert(60, moisture <= 1.0 && moisture >= 0.0);
     const int temperatureWeight = 2;
     const int humidityWeight = 2;
     const int moistureWeight = 6;
@@ -60,7 +66,7 @@ double PlantStateColorCalculator::normalizeInputValues(double temperature, doubl
     // weighted mean
     double result = (temperature * temperatureWeight + moisture * moistureWeight + humidity * humidityWeight)
            / (temperatureWeight + moistureWeight + humidityWeight);
-    assert(result <= 1.0 && result >= 0.0);
+    assert(68, result <= 1.0 && result >= 0.0);
     return result;
 }
 
@@ -70,7 +76,7 @@ int PlantStateColorCalculator::getBlue(double percent) {
 
 int PlantStateColorCalculator::getRed(double percent) {
     // mapping to [1, 20] fit function range
-    assert(percent <= 1.0 && percent >= 0.0);
+    assert(78, percent <= 1.0 && percent >= 0.0);
     double percentInFitRange = percent * 19 + 1;
     double approximatedValueInFitRange = -0.000188196 * pow(percentInFitRange, 6)
                                          + 0.0117451 * pow(percentInFitRange, 5)
@@ -83,7 +89,8 @@ int PlantStateColorCalculator::getRed(double percent) {
     double approximatedValueInColorRange = (approximatedValueInFitRange - 1) * 256 / 19;
 
     int result = (int) round(approximatedValueInColorRange);
-    assert(result <= 255 && result >= 0);
+    assert(91, result <= 255 && result >= 0);
+    Serial.println();
     return result;
 }
 
@@ -94,7 +101,7 @@ int PlantStateColorCalculator::getRed(double percent) {
  */
 int PlantStateColorCalculator::getGreen(double percent) {
     // mapping to [1, 20] fit function range
-    assert(percent <= 1.0 && percent >= 0.0);
+    assert(103, percent <= 1.0 && percent >= 0.0);
     double percentInFitRange = percent * 19 + 1;
     double approximatedValueInFitRange = -0.00019766 * pow(percentInFitRange, 6)
                                          + 0.0122973 * pow(percentInFitRange, 5)
@@ -106,6 +113,6 @@ int PlantStateColorCalculator::getGreen(double percent) {
     // mapping to [0, 255] color range
     double approximatedValueInColorRange = (approximatedValueInFitRange - 1) * 256 / 19;
     int result = (int) round(approximatedValueInColorRange);
-    assert(result <= 255 && result >= 0);
+    assert(115, result <= 255 && result >= 0);
     return result;
 }
